b7db57a3cc3e5084eaeccbba75eed45b0c3beb4a
naoyuki <hoshizorarequest@gmail.com>
2022-05-27 18:53:03 +0900

    add_render_404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
   before_action :require_login
   add_flash_types :success, :info, :warning, :danger
 
@@ -7,5 +9,8 @@ class ApplicationController < ActionController::Base
   def not_authenticated
     redirect_to login_path, warning: t('defaults.require_login')
   end
-
+  
+  def render_404
+    render file: Rails.root.join("public/404.html"), status: 404, layout: false, content_type: "text/html"
+  end
 end
--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+  before_action :set_current_user_board, only: %i[edit update destroy]
 
   def index
     @boards = Board.all.includes(:user).order(created_at: :desc)
@@ -18,15 +19,35 @@ class BoardsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @board.update(board_params)
+      redirect_to @board, success: t('defaults.message.update', item: Board.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_update', item: Board.model_name.human)
+      render :edit
+    end
+  end
+
   def show
     @board = Board.find(params[:id])
     @comment = Comment.new
     @comments = @board.comments.includes(:user).order(created_at: :desc)
   end
 
+  def destroy
+    @board.destroy!
+    redirect_to boards_path, success: t('defaults.message.destroy', item: Board.model_name.human)
+  end
+
   private
 
   def board_params
     params.require(:board).permit(:title, :body, :board_image, :board_image_cache)
+  end
+
+  def set_current_user_board
+    @board = current_user.boards.find(params[:id])
   end
 end
