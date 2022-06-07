37171098c56b00091bb4923e97ed398ef946276a
naoyuki <hoshizorarequest@gmail.com>
2022-06-07 15:55:11 +0900

    20_profile_edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_30_074721) do
+ActiveRecord::Schema.define(version: 2022_06_07_054529) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_05_30_074721) do
     t.datetime "updated_at", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
